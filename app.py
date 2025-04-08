a6c4b78a113787213ad51acc59595e187a67074e
Owen Buttigieg <obuttigieg2@gmail.com>
2025-04-08 13:13:46 +0800

    Added Login and Signup skeleton

--- app.py
+++ app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template("html/index.html")
 
+@app.route('/login')
+def login():
+    return render_template('html/login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('html/signup.html')
+
+@app.route('/privacy')
+def privacy():
+    return render_template('html/privacy.html') 
+
+@app.route('/feedback')
+def feedback():
+    return render_template('html/feedback.html') 
+
 if __name__ == '__main__':
     app.run(debug=True)

d2fc856db6a0fb8ee7c6dbae2301abe936451ecf
23722943 <23722943@student.uwa.edu.au>
2025-05-06 23:06:03 +0800

    Create initial front page
    - Added working navbar and UI elements
    - Implemented dark mode toggle functionality
    - Added description section with 3D effects

--- app/create_app.py
+++ app/create_app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from instance.webforms import db
 from app.routes import configure_routes
-from .transactions import transactions_bp
-from .authentication import authentication_bp
-from .budgeting_and_goals import budgeting_and_goals_bp
-from .dashboard_and_analytics import dashboard_and_analytics_bp
+#from .transactions import transactions_bp
+#from .authentication import authentication_bp
+#from .budgeting_and_goals import budgeting_and_goals_bp
+#from .dashboard_and_analytics import dashboard_and_analytics_bp
 
 def create_app():
     app = Flask(__name__, static_url_path='/static/css')
@@ -20,10 +20,10 @@ def create_app():
     db.init_app(app)
 
     # Register blueprints
-    app.register_blueprint(transactions_bp, url_prefix='/transactions')
-    app.register_blueprint(authentication_bp, url_prefix='/authentication')
-    app.register_blueprint(budgeting_and_goals_bp, url_prefix='/budgeting_and_goals')
-    app.register_blueprint(dashboard_and_analytics_bp, url_prefix='/dashboard_and_analytics')
+    #app.register_blueprint(transactions_bp, url_prefix='/transactions')
+    #app.register_blueprint(authentication_bp, url_prefix='/authentication')
+    #app.register_blueprint(budgeting_and_goals_bp, url_prefix='/budgeting_and_goals')
+    #app.register_blueprint(dashboard_and_analytics_bp, url_prefix='/dashboard_and_analytics')
 
     # Configure routes
     configure_routes(app)
